8c7b1d9298c4bd91c6560aac1201eb91dfd633e3
natasajevtic <nataly.jevtic.95@gmail.com>
2020-08-05 12:08:48 +0200

    UI and logic for login and registration created

---
+++ DAN_L_Natasa_Jevtic/Zadatak_1/ViewModels/UserViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zadatak_1.Models;
+using Zadatak_1.Views;
+
+namespace Zadatak_1.ViewModels
+{
+    class UserViewModel : BaseViewModel
+    {
+        UserView userView;
+
+        private vwUser user;
+
+        public vwUser User
+        {
+            get
+            {
+                return user;
+            }
+            set
+            {
+                user = value;
+                OnPropertyChanged("User");
+            }
+        }
+
+        public UserViewModel(UserView userView, vwUser user)
+        {
+            this.userView = userView;
+            User = user;
+        }
+    }
+}
---
+++ DAN_L_Natasa_Jevtic/Zadatak_1/Views/UserView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using Zadatak_1.Models;
+using Zadatak_1.ViewModels;
+
+namespace Zadatak_1.Views
+{
+    /// <summary>
+    /// Interaction logic for UserView.xaml
+    /// </summary>
+    public partial class UserView : Window
+    {
+        public UserView(vwUser user)
+        {
+            InitializeComponent();
+            this.DataContext = new UserViewModel(this, user);
+        }
+    }
+}
