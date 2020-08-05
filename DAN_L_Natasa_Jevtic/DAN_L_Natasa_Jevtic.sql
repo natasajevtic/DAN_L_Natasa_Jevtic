27ca6cce97b012ef61bec6cc53ecdd94e8278bc3
natasajevtic <nataly.jevtic.95@gmail.com>
2020-08-05 11:10:16 +0200

    SQL script and Entity Data Model changed

--- DAN_L_Natasa_Jevtic/DAN_L_Natasa_Jevtic.sql
+++ DAN_L_Natasa_Jevtic/DAN_L_Natasa_Jevtic.sql
@@ -3,18 +3,18 @@ IF DB_ID('AudioPlayer') IS NULL
 GO	
 use AudioPlayer
 --Deleting tables and views, if they exist
-IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'tblUser')
-	drop table tblUser;
-IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'tblSong')
-	drop table tblSong;
 IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'tblPlayedSong')
 	drop table tblPlayedSong;
-IF EXISTS(select * FROM sys.views where name = 'vwUser')
-	drop view vwUser;
-IF EXISTS(select * FROM sys.views where name = 'vwSong')
-	drop view vwSong;
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'tblSong')
+	drop table tblSong;
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'tblUser')
+	drop table tblUser;
 IF EXISTS(select * FROM sys.views where name = 'vwPlayedSong')
 	drop view vwPlayedSong;
+IF EXISTS(select * FROM sys.views where name = 'vwSong')
+	drop view vwSong;
+IF EXISTS(select * FROM sys.views where name = 'vwUser')
+	drop view vwUser;
 GO
 create table tblUser(
 UserId int identity(1,1) PRIMARY KEY,
