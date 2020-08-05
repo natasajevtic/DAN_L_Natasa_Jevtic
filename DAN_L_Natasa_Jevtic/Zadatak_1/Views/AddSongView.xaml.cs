132e1f94c7cf3790ab60cc7a932fb575fd7fc594
natasajevtic <nataly.jevtic.95@gmail.com>
2020-08-05 13:37:01 +0200

    UI and logic for user created

---
+++ DAN_L_Natasa_Jevtic/Zadatak_1/Views/AddSongView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using Zadatak_1.Models;
+using Zadatak_1.ViewModels;
+
+namespace Zadatak_1.Views
+{
+    /// <summary>
+    /// Interaction logic for AddSongView.xaml
+    /// </summary>
+    public partial class AddSongView : Window
+    {
+        public AddSongView(vwUser user)
+        {
+            InitializeComponent();
+            this.DataContext = new AddSongViewModel(this, user);
+        }
+    }
+}
